2e16148a0af305f6764762d4fe875e5fe4fb2171
Soumyadip <nayaksoumyadip@gmail.com>
2022-02-09 21:24:18 +0530

    Test Commit

--- Test/List.py
+++ Test/List.py
@@ -1,5 +1,6 @@
 # Building and using a list
 list1 = []  # Declaring a list
-while true:
-    printf("Enter 1 to add item in grocery list, 2 to insert item in the middle, 3 to remove using item number, 4 to "
+while True:
+    print("Enter 1 to add item in grocery list, 2 to insert item in the middle, 3 to remove using item number, 4 to "
            "remove item using name, 5 to reverse the list, 6 to sort list in alphabetical(or numeric) order, ")
+    break
