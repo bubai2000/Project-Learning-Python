a534fd68d5c3cf2fd03cc0e41571b5dfbf3a041c
Soumyadip Nayak <nayaksoumyadip@gmail.com>
2022-02-28 11:55:16 +0530

    Digital display completion

--- Source/Digital Display.py
+++ Source/Digital Display.py
@@ -1,23 +1,23 @@
 #precedence 
-def seg1:
+def seg1():
   print(end=" ")
   for col in range(3):
     print("*",end="")
   print()
 
-def seg2:
+def seg2():
   for i in range(3):
     print("*")
   print()
 
-def seg3:
+def seg3():
   for i in range(3):
     for j in range(3):
       print(end=" ")
     print("*")
   print()
 
-def seg23:
+def seg23():
   for i in range(3):
     print("*",end="")
     for j in range(2):
@@ -25,25 +25,25 @@ def seg23:
     print("*")
   print()
 
-def seg4:
+def seg4():
   print(end=" ")
   for i in range(3):
     print("*",end="")
   print()
 
-def seg5:
+def seg5():
   for i in range(3):
     print("*")
   print()
 
-def seg6:
+def seg6():
   for i in range(3):
     for j in range(3):
       print(end=" ")
     print("*")
   print()
 
-def seg56:
+def seg56():
   for i in range(3):
     print("*",end="")
     for j in range(2):
@@ -51,7 +51,7 @@ def seg56:
     print("*")
   print()
 
-def seg7:
+def seg7():
   print(end=" ")
   for i in range(3):
     print("*",end="")
@@ -62,11 +62,54 @@ if(p==1):
   seg1()
   seg3()
   seg5()
-if(p==2):
+elif(p==2):
   seg1()
   seg3()
   seg4()
   seg5()
   seg7()
-
-#
+elif(p==3):
+  seg1()
+  seg3()
+  seg4()
+  seg6()
+  seg7()
+elif(p==4):
+  seg23()
+  seg4()
+  seg6()
+elif(p==5):
+  seg1()
+  seg2()
+  seg4()
+  seg6()
+  seg7()
+elif(p==6):
+  seg1()
+  seg3()
+  seg4()
+  seg56()
+  seg7()
+elif(p==7):
+  seg1()
+  seg3()
+  seg6()
+elif(p==8):
+  seg1()
+  seg23()
+  seg4()
+  seg56()
+  seg7()
+elif(p==9):
+  seg1()
+  seg23()
+  seg4()
+  seg6()
+  seg7()
+elif(p==0):
+  seg1()
+  seg23()
+  seg56()
+  seg7()
+else:
+  print("Enter a number in the range 0 to 9!")
